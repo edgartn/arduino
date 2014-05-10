1a5eb151ad2abc705720d49e7028195a4d6cc8e7
edgartn <edgartn@gmail.com>
2014-05-10 00:13:26 -0300

    Update FuelInjector.ino

--- FuelInjector.ino
+++ FuelInjector.ino
@@ -13,21 +13,42 @@
 int gACELERADOR  = 0; 
 int gBICOINJETOR = 6;
 int gRPM         = 85; // Ao dar a "partida" se inicia com 1000 RPM (1000/120 = 85,25)
+int gModo        = 7;  // Lendo a porta 7 para alternar entre o modo economico e o de performance.
+int gModoAtual   = 0;
+
 
 void setup() {
   Serial.begin(9600);
   pinMode(gBICOINJETOR, OUTPUT); 
+  pinMode(gModo, INPUT); 
 }
 
 void loop() {
   gRPM = analogRead(gACELERADOR) * 10; // Multiplicando valor por 10 para obter valor próximo aos RPM de um carro
+
+  gModoAtual = digitalRead(gModo);
   
-  AcionarBicoInjetor(gRPM/120); // RPM/120 => Pois é uma injeção a cada 120 segundos.
+  if (gModoAtual == HIGH) {
+    AcionarBicoInjetor(gRPM, 'p');
+  } else {
+    AcionarBicoInjetor(gRPM, 'e');
+  }
+  
   
   Serial.println(gRPM);
 }
 
-void AcionarBicoInjetor(int aRPM) {
+void AcionarBicoInjetor(int aRPM, String aModo) {
+
+  // Modo e = economômico => RPM/180 => Maior intervalo entre as injeções garantindo assim mais economia.
+  if (aModo = 'e') {
+    aRPM = aRPM/180;
+  }
+
+  // Modo p = performance => RPM/120 => Menor intervalo entre as injeções permitindo assim mais performance.
+  if (aModo = 'p'){
+    aRPM = aRPM/120;
+  }
 
   // Fixando em 20 o menor delay pois abaixo deste valor a "piscada" do led não é visível ao olho humano.
   if (aRPM < 20) {
